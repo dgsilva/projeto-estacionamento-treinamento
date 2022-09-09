a7352e0d901c91fca844687257994ac9a4575170
dgsilva <diegorj93@gmail.com>
2022-09-08 23:33:31 -0300

    implementação no metodo editar e paginação

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotControllers.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotControllers.java
@@ -2,13 +2,16 @@ package com.api.parkingcontrol.controllers;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,9 +59,8 @@ public class ParkingSpotControllers {
 	}
 	
 	@GetMapping()
-	public List<ParkingSpotModel> findAll(){
-		List<ParkingSpotModel>list = parkingSpotService.findAll();
-		return list;
+	public ResponseEntity<Page<ParkingSpotModel>> findAll(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable){
+		return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll(pageable));		
 	}
 	
 	@GetMapping("/{id}")
@@ -79,6 +82,22 @@ public class ParkingSpotControllers {
 		
 		parkingSpotService.delete(parkSpotModelOptional.get());
 		return ResponseEntity.status(HttpStatus.OK).body("Foi deletado a vaga no estacionamento com sucesso!");
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Object>updateParkinSpot(@PathVariable("id")UUID id,
+			@RequestBody @Valid ParkingSpotDTO parkingSpotDTO){
+		Optional <ParkingSpotModel> parkSpotModelOptional = parkingSpotService.findById(id);
+		if(!parkSpotModelOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não foi encontrado nenhuma vaga no estacionamento");
+			
+		}
+		
+		var parkingSpotModel = new ParkingSpotModel();
+		BeanUtils.copyProperties(parkingSpotDTO, parkingSpotModel);
+		parkingSpotModel.setId(parkSpotModelOptional.get().getId());
+		parkingSpotModel.setRegistrationDate(parkSpotModelOptional.get().getRegistrationDate());
+		return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.save(parkingSpotModel));
 	}
 	
 }
